9daed7f85fcb76723c59187308bfe0c73972620b
Dan Plows <plowdk51@gmail.com>
2018-11-04 16:39:59 -0500

    passing object to caster partial in prepartion for diff stats per player based on their pre game choices

---
+++ online-spell-game/Models/Caster.cs
@@ -0,0 +1,10 @@
+﻿namespace online_spell_game.Models
+{
+	public class Caster
+	{
+		public int num { get; set; }
+		public int hp { get; set; }
+		public int mana { get; set; }
+		public int ac { get; set; }
+	}
+}
